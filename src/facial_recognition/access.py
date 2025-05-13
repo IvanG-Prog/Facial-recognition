07a789398c060f6fbea489f8b4dea103473c56d8
Jose Contreras <contrerasgedler@gmail.com>
2025-05-13 10:18:50 -0400

    Wrap the scripts in a main fucntion

--- src/facial_recognition/access.py
+++ src/facial_recognition/access.py
@@ -6,112 +6,117 @@ import os
 import numpy as np
 import time
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # configuration
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # configuration
 
-model = InceptionResnetV1(pretrained='casia-webface').eval().to(device)  # load facenet model
+    model = InceptionResnetV1(pretrained='casia-webface').eval().to(device)  # load facenet model
 
-mtcnn = MTCNN(keep_all=True, margin=20, min_face_size=20, device=device)  # load mtcnn for face detection,
-
-
-#base_path = "/home/ivang/Ivan/Python/pytorch/facenet/resgister_faces"  # database path
-
-#check that register_faces exists
-if not os.path.exists('register_faces'):
-    print("The register_faces folder does not exist. Please run the registers.py script first.")
-    exit(1)
-# Absolute path of the current file
-current_file_path = os.path.abspath(__file__)
-# Go up three directories: facial_recognition → src → project root
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))
-# Path to the new folder at the project root level
-base_path = os.path.join(project_root, 'register_faces')
+    mtcnn = MTCNN(keep_all=True, margin=20, min_face_size=20, device=device)  # load mtcnn for face detection,
 
 
-wait_time= 300
+    #base_path = "/home/ivang/Ivan/Python/pytorch/facenet/resgister_faces"  # database path
 
-for attempt in range(3):
-   username = input("Enter your username to access: ")  # username request
-   user_dir = os.path.join(base_path, username)
+    #check that register_faces exists
+    if not os.path.exists('register_faces'):
+        print("The register_faces folder does not exist. Please run the registers.py script first.")
+        exit(1)
+    # Absolute path of the current file
+    current_file_path = os.path.abspath(__file__)
+    # Go up three directories: facial_recognition → src → project root
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))
+    # Path to the new folder at the project root level
+    base_path = os.path.join(project_root, 'register_faces')
 
-   if  os.path.exists(user_dir):  # check if the username isn't registered
-        break
-   else:
-        print("The user is not registered.")
-        if attempt < 2:
-            print("Please try again. You have",2 - attempt, "attempts left.")
+
+    wait_time= 300
+
+
+    for attempt in range(3):
+        username = input("Enter your username to access: ")  # username request
+        user_dir = os.path.join(base_path, username)
+
+        if  os.path.exists(user_dir):  # check if the username isn't registered
+            break
         else:
-            print("You have reached the maximum number of attempts. Please wait 5 minutes.")
-            time.sleep(wait_time)
-            exit
+            print("The user is not registered.")
+            if attempt < 2:
+                print("Please try again. You have",2 - attempt, "attempts left.")
+            else:
+                print("You have reached the maximum number of attempts. Please wait 5 minutes.")
+                time.sleep(wait_time)
+                exit
 
-image_path = os.path.join(user_dir, 'registered_face.jpg')  # path of the resgistered image
+    image_path = os.path.join(user_dir, 'registered_face.jpg')  # path of the resgistered image
 
-if not os.path.exists(image_path):  # check if there is a registered image
-    print("No registered image found.")
-    exit(1)
+    if not os.path.exists(image_path):  # check if there is a registered image
+        print("No registered image found.")
+        exit(1)
 
-registered_image = Image.open(image_path)  # open the image with PIL
+    registered_image = Image.open(image_path)  # open the image with PIL
 
-registered_boxes = mtcnn(registered_image)  # boxes will contain the coordinates of the detected faces
+    registered_boxes = mtcnn(registered_image)  # boxes will contain the coordinates of the detected faces
 
-if registered_boxes is None:   # check if any faces were detected
-    print("No face was detected in the recorded image.")
-    exit(1)
+    if registered_boxes is None:   # check if any faces were detected
+        print("No face was detected in the recorded image.")
+        exit(1)
 
-registered_embedding = model(registered_boxes).detach().cpu().numpy() #passes the face images through the FaceNet model to obtain a feature vector
-
-
-def take_photo_and_show():  #function to take a photo
-    cap = cv2.VideoCapture(0)  # open the default camare (0 is the index)
-    if not cap.isOpened():
-        print("HE CAMARE COULD NOT BE OPEN.")
-        return None
-
-    while True:  # Loop for Video Capture
-        ret, frame = cap.read()  # ?????
-        if not ret:
-            print("HE PHOTO COULD NOT BE TAKEN.")
-            break
-
-        cv2.imshow('Press "s" to take the photo or "q" to exit', frame)
-
-        # wait for the user to press 's' or 'q'
-        key = cv2.waitKey(1)
-        if key == ord('s'):
-            new_image_path = os.path.join(user_dir, 'login_face.jpg')  # path for the image
-            cv2.imwrite(new_image_path, frame)  # load image in the path
-            print("Photo saved as:", new_image_path)
-            break
-        elif key == ord('q'):
-            print("Departure canceled.")
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()  # closed opencv window
+    registered_embedding = model(registered_boxes).detach().cpu().numpy() #passes the face images through the FaceNet model to obtain a feature vector
 
 
-take_photo_and_show()   # function call
+    def take_photo_and_show():  #function to take a photo
+        cap = cv2.VideoCapture(0)  # open the default camare (0 is the index)
+        if not cap.isOpened():
+            print("HE CAMARE COULD NOT BE OPEN.")
+            return None
 
-new_image = Image.open(os.path.join(user_dir, 'login_face.jpg'))  # open the new image
+        while True:  # Loop for Video Capture
+            ret, frame = cap.read()  # ?????
+            if not ret:
+                print("HE PHOTO COULD NOT BE TAKEN.")
+                break
 
-new_boxes = mtcnn(new_image)  #boxes will contain the coordinates of the detected faces
+            cv2.imshow('Press "s" to take the photo or "q" to exit', frame)
 
-if new_boxes is None:   # check if any faces were detected
-    print("No face was detected in the recorded image.")
-    exit(1)
+            # wait for the user to press 's' or 'q'
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                new_image_path = os.path.join(user_dir, 'login_face.jpg')  # path for the image
+                cv2.imwrite(new_image_path, frame)  # load image in the path
+                print("Photo saved as:", new_image_path)
+                break
+            elif key == ord('q'):
+                print("Departure canceled.")
+                break
 
-new_embedding = model(new_boxes).detach().cpu().numpy()  #passes the face images through the FaceNet model to obtain a feature vector
+        cap.release()
+        cv2.destroyAllWindows()  # closed opencv window
 
 
-def compare_embeddings(embedding1, embedding2):  # function to compare embeddings using the euclidean distance
-    distance = np.linalg.norm(embedding1 - embedding2)
-    return distance
+    take_photo_and_show()   # function call
 
-distance = compare_embeddings(registered_embedding[0], new_embedding[0])  # function call
+    new_image = Image.open(os.path.join(user_dir, 'login_face.jpg'))  # open the new image
 
-threshold = 0.6  # adjust if necessary
+    new_boxes = mtcnn(new_image)  #boxes will contain the coordinates of the detected faces
 
-if distance < threshold:
-    print("Access granted.")
-else:
-    print("Access denied. The images do not mach.")
+    if new_boxes is None:   # check if any faces were detected
+        print("No face was detected in the recorded image.")
+        exit(1)
+
+    new_embedding = model(new_boxes).detach().cpu().numpy()  #passes the face images through the FaceNet model to obtain a feature vector
+
+
+    def compare_embeddings(embedding1, embedding2):  # function to compare embeddings using the euclidean distance
+        distance = np.linalg.norm(embedding1 - embedding2)
+        return distance
+
+    distance = compare_embeddings(registered_embedding[0], new_embedding[0])  # function call
+
+    threshold = 0.6  # adjust if necessary
+
+    if distance < threshold:
+        print("Access granted.")
+    else:
+        print("Access denied. The images do not mach.")
+
+if __name__ == "__main__":
+    main()
--- src/facial_recognition/registers.py
+++ src/facial_recognition/registers.py
@@ -6,242 +6,137 @@ import os
 import re
 
 
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # configuration
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # configuration
+    model = InceptionResnetV1(pretrained='casia-webface').eval().to(device)  # load facenet model
 
-model = InceptionResnetV1(pretrained='casia-webface').eval().to(device)  # load facenet model
+    mtcnn = MTCNN(keep_all=True,margin= 20 ,min_face_size=20, device=device)  # load mtcnn for face detection,
 
-mtcnn = MTCNN(keep_all=True,margin= 20 ,min_face_size=20, device=device)  # load mtcnn for face detection,
+    # user dates
+    def valid_name_lastname(input_data):
+        return bool(re.match("^[A-Za-záéíóúÁÉÍÓÚñÑ ]+$", input_data))
 
-# user dates
-def valid_name_lastname(input_data):
-    return bool(re.match("^[A-Za-záéíóúÁÉÍÓÚñÑ ]+$", input_data))
+    def valid_ID(input_data):
+        return input_data.isdigit()
 
-def valid_ID(input_data):
-    return input_data.isdigit()
+    def valid_username(input_data):
+        return bool(re.search("[A-Za-z !@#$%^&*()_+]+", input_data)) and bool(re.search("[0-9]", input_data))
 
-def valid_username(input_data):
-    return bool(re.search("[A-Za-z !@#$%^&*()_+]+", input_data)) and bool(re.search("[0-9]", input_data))
+    def register():
 
-def register():
+        first_names= input("Enter your names: ")
+        while not valid_name_lastname(first_names):
+            print('Error: Names must only contain letters. Please try again.')
+            first_names= input('Enter your names: ')
 
-    first_names= input("Enter your names: ")
-    while not valid_name_lastname(first_names):
-        print('Error: Names must only contain letters. Please try again.')
-        first_names= input('Enter your names: ')
-
-    last_name= input("Enter your last name: ")
-    while not valid_name_lastname(last_name):
-        print('Error: last names must only contain letters. Please try again.')
         last_name= input("Enter your last name: ")
+        while not valid_name_lastname(last_name):
+            print('Error: last names must only contain letters. Please try again.')
+            last_name= input("Enter your last name: ")
 
-    identity_card = input("Enter your identy card: ")
-    while not valid_ID(identity_card):
-        print("ERROR: The ID card must only contain numbers. Please try again.")
         identity_card = input("Enter your identy card: ")
+        while not valid_ID(identity_card):
+            print("ERROR: The ID card must only contain numbers. Please try again.")
+            identity_card = input("Enter your identy card: ")
 
-    username = input("Choose a username: ")
-    while not valid_username(username):
-        print("Error: Username must contain at least one symbol or number. Please try again.")
         username = input("Choose a username: ")
+        while not valid_username(username):
+            print("Error: Username must contain at least one symbol or number. Please try again.")
+            username = input("Choose a username: ")
 
-    return  first_names, last_name, identity_card, username
+        return  first_names, last_name, identity_card, username
 
-first_names, last_name, identity_card, username= register()
+    first_names, last_name, identity_card, username= register()
 
-# Absolute path of the current file
-current_file_path = os.path.abspath(__file__)
+    # Absolute path of the current file
+    current_file_path = os.path.abspath(__file__)
 
-# Go up three directories: facial_recognition → src → project root
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))
+    # Go up three directories: facial_recognition → src → project root
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))
 
-# Path to the new folder at the project root level
-base_path = os.path.join(project_root, 'register_faces')
+    # Path to the new folder at the project root level
+    base_path = os.path.join(project_root, 'register_faces')
 
-# Create the folder if it doesn't exist
-os.makedirs(base_path, exist_ok=True)
+    # Create the folder if it doesn't exist
+    os.makedirs(base_path, exist_ok=True)
 
-if not os.path.exists(base_path):  # check if the folder exists
-    os.makedirs(base_path)  # create the folder if it doesn't exist
+    if not os.path.exists(base_path):  # check if the folder exists
+        os.makedirs(base_path)  # create the folder if it doesn't exist
 
-user_dir = os.path.join(base_path, username)  # concatenates the path and the username
+    user_dir = os.path.join(base_path, username)  # concatenates the path and the username
 
-while os.path.exists(user_dir):  # check if the name exists
-    print('Username not available', username, 'please choose another')
-    username= input('Choose a username: ')
-    user_dir = os.path.join(base_path, username)
+    while os.path.exists(user_dir):  # check if the name exists
+        print('Username not available', username, 'please choose another')
+        username= input('Choose a username: ')
+        user_dir = os.path.join(base_path, username)
 
-os.makedirs(user_dir, exist_ok=True)  # Create a folder
+    os.makedirs(user_dir, exist_ok=True)  # Create a folder
 
 
-def take_photo_and_show():  # function to take a photo
-    cap = cv2.VideoCapture(0)  # open the default camare (0 is the index)
+    def take_photo_and_show():  # function to take a photo
+        cap = cv2.VideoCapture(0)  # open the default camare (0 is the index)
 
-    if not cap.isOpened():  # check if the camere has opened
-        print("THE CAMARE COULD NOT BE OPEN.")
-        return None
+        if not cap.isOpened():  # check if the camere has opened
+            print("THE CAMARE COULD NOT BE OPEN.")
+            return None
 
-    while True:  # Loop for Video Capture
-        ret, frame = cap.read()  # ?????
-        if not ret:
-            print("THE PHOTO COULD NOT BE TAKEN.")
-            break
+        while True:  # Loop for Video Capture
+            ret, frame = cap.read()  # ?????
+            if not ret:
+                print("THE PHOTO COULD NOT BE TAKEN.")
+                break
 
-        cv2.imshow('Press "s" to take the photo or "q" to exit', frame)
+            cv2.imshow('Press "s" to take the photo or "q" to exit', frame)
 
-        # wait for the user to press 's' or 'q'
-        key = cv2.waitKey(1)
-        if key == ord('s'):
-            filename = os.path.join(user_dir, 'registered_face.jpg')  # path for the image
-            cv2.imwrite(filename, frame)  # load image in the path
-            print("Photo saved as:", filename)
-            break
-        elif key == ord('q'):
-            print("Departure canceled.")
-            break
+            # wait for the user to press 's' or 'q'
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                filename = os.path.join(user_dir, 'registered_face.jpg')  # path for the image
+                cv2.imwrite(filename, frame)  # load image in the path
+                print("Photo saved as:", filename)
+                break
+            elif key == ord('q'):
+                print("Departure canceled.")
+                break
 
-    cap.release()  # closed the camare
-    cv2.destroyAllWindows()  # closed opencv window
+        cap.release()  # closed the camare
+        cv2.destroyAllWindows()  # closed opencv window
 
-take_photo_and_show()  # function call
+    take_photo_and_show()  # function call
 
 
-image_path = os.path.join(user_dir, 'registered_face.jpg')  # path
+    image_path = os.path.join(user_dir, 'registered_face.jpg')  # path
 
-image = Image.open(image_path)  # open image
-boxes = mtcnn(image)  # boxes will contain the coordinates of the detected faces
+    image = Image.open(image_path)  # open image
+    boxes = mtcnn(image)  # boxes will contain the coordinates of the detected faces
 
-if boxes is not None:  # check if any faces were detected
+    if boxes is not None:  # check if any faces were detected
 
-    img_cropped = mtcnn(image)  # extracts the face
-    embeddings = model(img_cropped).detach().cpu().numpy()  #passes the face images through the FaceNet model to obtain a feature vector
+        img_cropped = mtcnn(image)  # extracts the face
+        embeddings = model(img_cropped).detach().cpu().numpy()  #passes the face images through the FaceNet model to obtain a feature vector
 
-    for i in embeddings:  #for i, embedding in enumerate(embeddings):
+        for i in embeddings:  #for i, embedding in enumerate(embeddings):
 
-        print('Feature vector guardado en', image_path) #print('Feature vector' ,i + 1 , 'guardado en', image_path)
-        print(i)
-else:
-    print("No face was detected.")
+            print('Feature vector guardado en', image_path) #print('Feature vector' ,i + 1 , 'guardado en', image_path)
+            print(i)
+    else:
+        print("No face was detected.")
 
 
-data_file_path = os.path.join(user_dir, 'user_data.txt')  # created a text file to save the data
+    data_file_path = os.path.join(user_dir, 'user_data.txt')  # created a text file to save the data
 
-with open(data_file_path, 'w') as data_file:
-    data_file.write("Name: " + first_names)
-    data_file.write("Last name: " + last_name)
-    data_file.write("Identity card: " + identity_card)
-    data_file.write("Username: " +   username)
+    with open(data_file_path, 'w') as data_file:
+        data_file.write("Name: " + first_names)
+        data_file.write("Last name: " + last_name)
+        data_file.write("Identity card: " + identity_card)
+        data_file.write("Username: " +   username)
 
-print("Username data saved in: ", data_file_path)
-print("Registration completed")
+    print("Username data saved in: ", data_file_path)
+    print("Registration completed")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
 
 
